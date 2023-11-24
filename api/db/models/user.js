fa577bc15cfb5c1b7a96416801cfba961a147df9
igor <e-baristae-barista@yandex.ru>
2023-11-24 16:37:42 +0300

    rel3

--- api/db/models/user.js
+++ api/db/models/user.js
@@ -1,15 +1,15 @@
-'use strict';
-const { Model } = require('sequelize');
+const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-      /**
+    /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-      static associate(models) {
-        // define association here
-      }
+    static associate(models) {
+      // define association here
+    }
   }
   User.init(
     {
@@ -20,8 +20,8 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: 'User',
-    },
+      modelName: "User",
+    }
   );
   return User;
 };
