3291f788eb9913e3d2c30f5f8236838e5553ae66
Vallydei <obedkova.v@rambler.ru>
2023-11-24 11:56:04 +0300

    questPage, locationPage and Logs merged

--- client/src/components/pages/LoginPage.tsx
+++ client/src/components/pages/LoginPage.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-
-import { Form } from 'react-router-dom';
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import type { LoginFormData } from '../../types/auth';
 import { useAppDispatch } from '../../redux/hooks';
 import { thunkLogin } from '../../redux/slices/auth/createAsyncThunks';
@@ -9,23 +7,45 @@ import { thunkLogin } from '../../redux/slices/auth/createAsyncThunks';
 export default function LoginPage(): JSX.Element {
   const dispatch = useAppDispatch();
   return (
-    <Form
+    <form
       onSubmit={(e) => {
         e.preventDefault();
         const formData = Object.fromEntries(new FormData(e.currentTarget)) as LoginFormData;
         void dispatch(thunkLogin(formData));
       }}
     >
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" name="email" placeholder="Enter email" />
-      </Form.Group>
+      <h1>Войти</h1>
+      <TextField
+        required
+        label="E-mail"
+        placeholder="E-mail"
+        error={false && 'Invalid email address or password'}
+        name="email"
+      />
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" name="password" placeholder="Password" />
-      </Form.Group>
-      <Button type="submit">Submit</Button>
-    </Form>
+      <TextField
+        required
+        label="Password"
+        placeholder="Password"
+        error={false && 'Invalid email address or password'}
+        name="password"
+      />
+      <div>
+        <Button type="submit" color="success" variant="contained" size="large">
+          Submit
+        </Button>
+      </div>
+    </form>
   );
 }
+// {/* <Form.Group className="mb-3" controlId="formBasicEmail">
+//   <Form.Label>Email address</Form.Label>
+//   <Form.Control type="email" name="email" placeholder="Enter email" />
+// </Form.Group> */}
+
+// // eslint-disable-next-line no-lone-blocks
+// {/* <Form.Group className="mb-3" controlId="formBasicPassword">
+//   <Form.Label>Password</Form.Label>
+//   <Form.Control type="password" name="password" placeholder="Password" />
+// </Form.Group>
+// <Button type="submit">Submit</Button> */}
