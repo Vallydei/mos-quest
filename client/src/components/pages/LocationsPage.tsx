2ee5889c20b3c3e369d6e3aebb58356c6c9257c4
ytka98 <rickda1ton@yandex.ru>
2023-11-27 15:01:09 +0300

    LocationPage изменили

--- client/src/components/pages/LocationsPage.tsx
+++ client/src/components/pages/LocationsPage.tsx
@@ -1,15 +1,16 @@
-import React from "react";
-import RecipeReviewCard from "../ui/LocationsCard";
-import "./LocationsPage.css"; // Import the CSS file for styling
+import React from 'react';
+import RecipeReviewCard from '../ui/LocationsCard';
+import './LocationsPage.css'; // Import the CSS file for styling
 
 export default function LocationsPage(): JSX.Element {
   return (
-    <div className="locations-container">
-      <RecipeReviewCard />
-      <RecipeReviewCard />
-      <RecipeReviewCard />
-      <RecipeReviewCard />
-      <RecipeReviewCard />
+    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '10px' }}>
+      {locations?.map((location) => <RecipeReviewCard key={location?.id} location={location} />)}
+      {/* <RecipeReviewCard />
+        <RecipeReviewCard />
+        <RecipeReviewCard />
+        <RecipeReviewCard />
+        <RecipeReviewCard /> */}
     </div>
   );
 }
