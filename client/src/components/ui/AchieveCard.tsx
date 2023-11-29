5c8d34a2d705ed5f3d2ed6b554fbe02f47c78810
igor <e-baristae-barista@yandex.ru>
2023-11-29 18:40:46 +0300

    tooltip

--- client/src/components/ui/AchieveCard.tsx
+++ client/src/components/ui/AchieveCard.tsx
@@ -1,49 +1,38 @@
 import React, { useState } from 'react';
-import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
-import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+
 import { styled } from '@mui/material/styles';
 import type { AchieveType } from '../../types/locationType/achievesType';
 import '../pages/css/test.css';
-
 
 type AchieveCardProps = {
   achieve: AchieveType;
   getedAchievs: number[];
 };
 
-
-
 export default function AchieveCard({ achieve, getedAchievs }: AchieveCardProps): JSX.Element {
-
   const CustomWidthTooltip = styled(Tooltip)({
     maxWidth: 500,
     '& .MuiTooltip-tooltip': {
       fontSize: '16px',
       backgroundColor: '#F0F0F0',
-      
     },
   });
 
-  const longText = `
+  /* const longText = `
    Здесь должно быть описание за что можно получить эту награду
-  `;
- 
+  `;/////////// */
+
   return (
-    <div className={`${
-      (getedAchievs.includes(achieve.id)) ? 'getedAchiev' : 'achieveContainer'
-    }`}>
-    
-    <CustomWidthTooltip title={longText}>
+    <div className={`${getedAchievs.includes(achieve.id) ? 'getedAchiev' : 'achieveContainer'}`}>
+      <CustomWidthTooltip title={achieve.longText}>
         <p>{achieve.description}</p>
       </CustomWidthTooltip>
       <img
-        className={`achieveImg ${
-          (getedAchievs.includes(achieve.id)) ? 'achieveImg' : 'disabledImg'
-        }`}
+        className={`achieveImg ${getedAchievs.includes(achieve.id) ? 'achieveImg' : 'disabledImg'}`}
         src={achieve.img}
         alt="достижение"
       />
-     
     </div>
   );
-}  
+}
